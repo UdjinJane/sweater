4368ce9c3e3f6972299dc345e522a66935abb12b
Eugene Kondratov <udjin_jane@mail.ru>
2020-10-29 17:59:33 +1000

    Пашет авторизация

---
+++ src/main/java/ru/udjin/sweater/domain/Role.java
@@ -0,0 +1,5 @@
+package ru.udjin.sweater.domain;
+
+public enum Role {
+  USER;
+}
---
+++ src/main/java/ru/udjin/sweater/repos/UserRepo.java
@@ -0,0 +1,11 @@
+package ru.udjin.sweater.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.udjin.sweater.domain.User;
+
+
+public interface UserRepo extends JpaRepository<User, Long> {
+  User findByUsername(String username);
+
+
+}
